6f096577b3db233c6aa05b40d8d2957e0579de0a
Nikita Burinsky <nikburinsky@mail.ru>
2026-02-28 02:42:06 +0300

    Update SecurityConfig.java

--- src/main/java/nto/infrastructure/config/SecurityConfig.java
+++ src/main/java/nto/infrastructure/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .csrf(csrd -> csrd.disable())
             .cors(cors -> cors.configurationSource(request -> {
                 var corsConfiguration = new CorsConfiguration();
                 corsConfiguration.setAllowedOriginPatterns(List.of("*"));
